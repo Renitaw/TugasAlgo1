9b98faf3ea391d0127aa653c7825c631e0f79efe
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2022-10-06 19:02:17 -0700

    class program

--- TugasAlgo1/Program.cs
+++ TugasAlgo1/Program.cs
@@ -1,2 +1,13 @@
 ﻿using System;
 
+namespace Tugas1
+{
+    class program
+    {
+        // Deklarasi array int dengan ukuran 20
+        private int[] a = new int[20];
+
+        // Deklarasi variabel int untuk menyimpan banyaknya data pada array
+        private int n;
+
+        // Fungsi / Method untuk menerima masukan
