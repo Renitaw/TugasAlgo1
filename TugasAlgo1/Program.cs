9c89a80c5cde1f52193082bd79a29e9d5d6e0664
R Widiastuti <r.widiastuti.ft21@mail.umy.ac.id>
2022-10-06 19:05:48 -0700

    public void  bubble

--- TugasAlgo1/Program.cs
+++ TugasAlgo1/Program.cs
@@ -37,5 +37,36 @@ namespace Tugas1
                 a[i] = Int32.Parse(s1);
             }
         }
+        public void display()
+        {
+            // menampilkan array yang tersusun
+            Console.WriteLine("");
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("Element array yang telah tersusun");
+            Console.WriteLine("---------------------------------");
+            for (int j = 0; j < n; j++)
+            {
+                Console.WriteLine(a[j]);
+            }
+            Console.WriteLine("");
+        }
+        public void BubbleSortArray()
+        {
+            for (int i = 1; i < n; i++) // for n - 1 passes
+            {
+                // Pada pass i, bandingkan n - i elemen pertama dengan elemen selanjutnya
+                for (int j = 0; j < n - i; j++)
+                {
+                    if (a[j] > a[j + 1]) // Jika elemen tidak dalam urutan yang benar
+                    {
+                        // Tukar elemen
+                        int temp;
+                        temp = a[j];
+                        a[j] = a[j + 1];
+                        a[j + 1] = temp;
+                    }
+                }
+            }
+        }
     }
 }
